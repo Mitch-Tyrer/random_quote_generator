5c2194db2710fb279259a9e74ef2cc2578c83fd4
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-28 19:19:28 -0700

    started a random color function. need to pull math var out of that function and make it it's own

--- js/script.js
+++ js/script.js
@@ -1,108 +1,81 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 1 - A Random Quote Generator
-******************************************/
-
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+//Random Quote Generator
 
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-
-  Recommended: 
-    - Add at least one `year` and/or `citation` property to at least one 
-      quote object.
-***/
+// Variables
 
 var quotes = [
-    {
-      quote: "Choose a job you love, and you will never have to work a day in your life",
-      source: "Confucius"
-    },
-    {
-      quote: "But it ain’t about how hard you hit, it’s about how hard you can get hit and keep moving forward, how much can you take and keep moving forward",
-      source: "Rocky Balboa",
-      citation: "Rocky Balboa",
-      year: "2006"
-      
-    },
-    {
-      quote: "Every man’s life ends the same way. It is only the details of how he lived and how he died that distinguish one man from another." ,
-      source: "Ernest Hemingway",
-      year: "unknown"
-    },
-    {
-      quote: "All we have to decide is what to do with the time that is given to us.",
-      source: "Gandalf",
-      citation: "The Lord of the Rings: The Fellowship of the Ring",
-      year: "2001"
-    },
-    {
-      quote: "Monsters are real, and ghosts are real too. They live inside us, and sometimes, they win.",
-      source: "Stephen King"
-    },
+  {
+    quote: "Choose a job you love, and you will never have to work a day in your life",
+    source: "Confucius"
+  },
+  {
+    quote: "But it ain’t about how hard you hit, it’s about how hard you can get hit and keep moving forward, how much can you take and keep moving forward",
+    source: "Rocky Balboa",
+    citation: "Rocky Balboa",
+    year: "2006"
+
+  },
+  {
+    quote: "Every man’s life ends the same way. It is only the details of how he lived and how he died that distinguish one man from another.",
+    source: "Ernest Hemingway",
+    year: "unknown"
+  },
+  {
+    quote: "All we have to decide is what to do with the time that is given to us.",
+    source: "Gandalf",
+    citation: "The Lord of the Rings: The Fellowship of the Ring",
+    year: "2001"
+  },
+  {
+    quote: "Monsters are real, and ghosts are real too. They live inside us, and sometimes, they win.",
+    source: "Stephen King"
+  },
 
 ];
 
 
-
-
-
-
-/***
-  Create the `getRandomQuote` function to:
-   - generate a random number 
-   - use the random number to `return` a random quote object from the 
-     `quotes` array.
-***/
+// generate a random quote out of the array
 const getRandomQuote = (arr) => {
   let randomNumber = Math.floor(Math.random() * arr.length);
   return arr[randomNumber];
-  
+
+}
+
+// generate random background color
+const randomColor = () => {
+    //grab a random number between 0 and 255
+    let randomRGB = Math.floor(Math.random() * 256);
+    //place random number into rgbs
+    let rgb = 'background-color: rgb(' + randomRGB + ', ' + randomRGB + ', ' + randomRGB + ')';
+    //apply rgb to body css
+    let changeColor = document.querySelector('body');
+    return changeColor.setAttribute('style', rgb);
 }
 
 
 
-
-/***
-  Create the `printQuote` function to: 
-   - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to 
-     create your HTML string.
-   - use conditionals to make sure the optional properties exist before 
-     they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
+//Print quote to screen function
 const printQuote = () => {
-    let curQuote = getRandomQuote(quotes);
-    let output = document.getElementById('quote-box');
-    let html = '';
-    html+= '<p  class="quote">' + curQuote.quote + '</p>';
-    html+= '<p class="source">' + curQuote.source + '</p>';
-      if(curQuote.hasOwnProperty('citation')){
-        html += '<span class="citation">' + curQuote.citation + '</span>';
-      }
-      if(curQuote.hasOwnProperty('year')){
-        html += '<span class="year">' + curQuote.year + '</span>';
-      }
-      
-
-    return output.innerHTML = html;
+  randomColor();
+  let curQuote = getRandomQuote(quotes);
+  let output = document.getElementById('quote-box');
+  let html = '';
+  html += '<p  class="quote">' + curQuote.quote + '</p>';
+  html += '<p class="source">' + curQuote.source + '</p>';
+  if (curQuote.hasOwnProperty('citation')) {
+    html += '<span class="citation">' + curQuote.citation + '</span>';
+  }
+  if (curQuote.hasOwnProperty('year')) {
+    html += '<span class="year">' + curQuote.year + '</span>';
+  }
+  return output.innerHTML = html;
 }
 
 
 
 
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
+
+//button event to change quotes
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
